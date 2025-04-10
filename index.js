2f241c5736585d6488585fb86e27ffb9b6c6f4e8
Aun-Ali-12 <aunali29331@gmail.com>
2025-04-10 22:37:18 +0500

    questions done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //Find the sum of all elements in an array:
 
 const arr = [1, 2, 3, 4];
-let sum = arr[0] + arr[1] + arr[2] + arr[3];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  sum = sum + arr[i];
+}
 console.log(sum); //output 10
 
-//Find the max and min value in an array:
+// Find the max and min value in an array:
 
 const anotherArr = [4, 9, 2, 7];
 let maxValInArr = Math.max(...anotherArr);
@@ -13,28 +16,27 @@ let minValInArr = Math.min(...anotherArr);
 console.log(`Maximum Value: ${minValInArr}`); //2
 
 //Count numbers of even and odd:
-function evenAndOddCount(arr) {
-  let evenCount = 0;
-  let oddCount = 0;
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      evenCount++;
-    } else {
-      oddCount++;
-    }
+let arr1 = [1, 2, 3, 4, 5]
+
+let even = 0; //1 => 2 = 2
+let odd = 0; //1 => 2 => 3 = 3
+for(let i = 0; i<arr1.length; i++){
+  if( arr[i]%2 === 0 ){
+   even++;
+  } else {
+    odd++;
   }
-  return { even: evenCount, odd: oddCount };
 }
-const nums = [1, 2, 3, 4, 5];
-let result = evenAndOddCount(nums);
-console.log(`Even count: ${result.even} and Odd count: ${result.odd}`);
+console.log(`Even Count: ${even} and Odd Count: ${odd}`);
 
-//Reverse an array manually(without using reverse method)
-const reverseArr = [1, 2, 3];
-// let length = reverseArr.length-1
-// console.log(length);
-console.log([reverseArr[2], reverseArr[1], reverseArr[0]]); //[ 3, 2, 1 ]
+//Printing an array in a reverse order using for loop:
+let reverseArr = [1, 2, 3];
+console.log("Reverse Array:");
+for (let i = reverseArr.length - 1; i > -1; i--) {
+  let reverseVal = reverseArr[i];
+  console.log(reverseVal);
+}
 
-//Check if the element (7) exists in an array:
+// Check if the element (7) exists in an array:
 const elementExistInArr = [5, 7, 9];
 console.log(elementExistInArr.includes(7)); //true
